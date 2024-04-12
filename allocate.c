6d17707508c1bfdae0194251e8167b39af0af066
Randra <kpardewo@student.unimelb.edu.au>
2024-04-12 15:25:12 +1000

    Created functions to read command line arguments and read processes within the specified file

--- allocate.c
+++ allocate.c
@@ -2,33 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*******************************************************************************************************/
 // List of Constants
 /* All process-names will be distinct uppercase alphanumeric strings 
     of minimum length 1 and maximum length 8. (+1 for space)*/
-#define MAX_PROCESS_NAME_LEN 9; 
+#define MAX_PROCESS_NAME_LEN 9 
+#define MAX_FILENAME_STRATEGY_LEN 100
 
 /* Initial size of dynamic array storing processes */
-#define INITIAL_PROCESSES 2;
+#define INITIAL_PROCESSES 2
 
 /* Defined states of a process */
-#define READY 0;
-#define RUNNING 1;
-#define FINISHED 2;
+#define READY 0
+#define RUNNING 1
+#define FINISHED 2
 
-// List of Functions
-void readInput(int argc, char* argv[], char** filename, char** memoryStrategy, int* quantum);
-Process* readProcesses(const char* filename, int* processCount);
-
-int main(int argc, char* argv[]) {
-
-    int currentState = READY;
-    return 0;
-}
-
+/*******************************************************************************************************/
 /* Process Structure Definition 
 */
-typedef struct {
-    int time_arrived;
+typedef struct Process {
+    int arrivalTime;
     char processName[MAX_PROCESS_NAME_LEN];
     int serviceTime;
     int memoryRequirement;
@@ -36,27 +29,53 @@ typedef struct {
     int cpuTimeUsed;
 } Process;
 
+/*******************************************************************************************************/
+// List of Functions
+void readInput(int argc, char* argv[], char filename[], char memoryStrategy[], int* quantum);
+Process* readProcesses(char filename[], int* processCount);
+
+/*******************************************************************************************************/
+int main(int argc, char* argv[]) {
+    char filename[MAX_FILENAME_STRATEGY_LEN], memoryStrategy[MAX_FILENAME_STRATEGY_LEN];
+    int quantum;
+
+    /* read command line arguments for simulation specifications */
+    readInput(argc, argv, filename, memoryStrategy, &quantum);
+    printf("filename: %s, memory strategy: %s, quantum: %d\n", filename, memoryStrategy, quantum);
+
+    /* read and store processes to be simulated */
+    int processCount;
+    Process* processes = readProcesses(filename, &processCount);
+    for (int i = 0; i < processCount; i++) {
+        printf("Process Name: %s, Arrival: %d, ServiceT: %d, Memory: %d, State: %d, CPU: %d\n",
+                processes[i].processName, processes[i].arrivalTime, processes[i].serviceTime,
+                processes[i].memoryRequirement, processes[i].state, processes[i].cpuTimeUsed);
+    }
+
+    return 0;
+}
+
 /* read command line arguments to deterimine:
     list of processes, memory strategy, & quantum length
 */
-void readInput(int argc, char* argv[], char** filename, char** memoryStrategy, int* quantum){
-    for (int i=1; i <= argc; i++) {
+void readInput(int argc, char* argv[], char filename[], char memoryStrategy[], int* quantum){
+    for (int i = 1; i < argc - 1; i++) {
         /* filename */
         if (strcmp(argv[i], "-f") == 0) {
-            filename = argv[++i];
+            strcpy(filename, argv[++i]);
         /* memory strategy (infinite, first-fit, paged, virtual)*/
         } else if (strcmp(argv[i], "-m") == 0) {
-            memoryStrategy = argv[++i];
+            strcpy(memoryStrategy, argv[++i]);
         /* quantum length (1, 2, 3)*/
         } else if (strcmp(argv[i], "-q") == 0) {
-            quantum = argv[++i];
+            *quantum = atoi(argv[++i]);
         }
     }
 }
 
 /* read list of processes from file and store their info
 */
-Process* readProcesses(const char* filename, int* processCount) {
+Process* readProcesses(char filename[], int* processCount) {
     /* open file in read, exit if not found */
     FILE *fp = fopen(filename, "r");
     if (fp == NULL) {
@@ -75,6 +94,28 @@ Process* readProcesses(const char* filename, int* processCount) {
 
     /* reading processes into the array */
     int arrival, serviceTime, memoryReq;
-    char name;
-    while (fscanf(fp, %d %s %d %d\n, &arrival, &name, &serviceTime, &memoryReq))
+    char name[MAX_PROCESS_NAME_LEN];
+    while (fscanf(fp, "%d %s %d %d", &arrival, &name, &serviceTime, &memoryReq) == 4) {
+        /* reallocate memory if needed */
+        if (*processCount == curentSize) {
+            curentSize *= 2;
+            processes = realloc(processes, sizeof(Process) * curentSize);
+            if (processes == NULL) {
+                fprintf(stderr, "Realloc failure: Process Array not reallocated in memory\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        /* Initialize process structure */
+        processes[*processCount].arrivalTime = arrival;
+        strcpy(processes[*processCount].processName, name);
+        processes[*processCount].serviceTime = serviceTime;
+        processes[*processCount].memoryRequirement = memoryReq;
+        processes[*processCount].state = READY;
+        processes[*processCount].cpuTimeUsed = READY;
+        (*processCount)++;
+    }
+
+    fclose(fp);
+    return processes;
 }
